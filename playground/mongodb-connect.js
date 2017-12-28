47c70563c77470d86ab4870b1076330919891a3f
Amy Etheredge <amy.etheredge@gmail.com>
2017-12-28 13:34:39 -0500

    init

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,37 @@
+const {MongoClient, ObjectId} = require('mongodb');
+
+// create object ID
+// var obj = new ObjectId();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017', (error, client) => {
+  if (error) {
+    return console.error('Unable to connect to database server.', error);
+  }
+  console.log('Connected to MongoDB server');
+
+  var db = client.db('cocktails');
+
+
+  // db.collection('spirits').insertOne({
+  //   'name': 'gin'
+  // }, (error, result) => {
+  //   if (error) {
+  //     return console.error('Unable to insert spirit', error);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2))
+  // });
+
+  // Insert new doc into Users collection (name, etc)
+
+  // db.collection('users').insertOne({
+  //   'name': 'Amy'
+  // }, (error, result) => {
+  //   if (error) {
+  //     return console.error('Unable to insert user.', error);
+  //   }
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
+
+  client.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,34 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+// create object ID
+// var obj = new ObjectID();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017', (error, client) => {
+  if (error) {
+    return console.error('Unable to connect to database server.', error);
+  }
+  console.log('Connected to MongoDB server');
+
+  var db = client.db('cocktails');
+
+  //.find() returns a 'cursor' (reference to data)
+  // db.collection('spirits').find({
+  //   _id: new ObjectID('5a45275a9eda4d74e1929799')
+  // }).toArray().then((docs) => {
+  //   console.log('spirits');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (error) => {
+  //   console.error('Unable to fetch spirits.', error);
+  // });
+
+  db.collection('spirits').find().count()
+    .then((count) => {
+      console.log(`Spirits count: ${count}`);
+    }, (error) => {
+      console.error('Spirits can not be counted.', error);
+    })
+
+
+  client.close();
+});
